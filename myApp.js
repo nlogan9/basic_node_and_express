610fb89269d3645f1f45fc77b5d03f7fd9f85756
talksalot9 <nathanlogan81@gmail.com>
2024-08-06 13:16:29 -0500

    test 3

--- myApp.js
+++ myApp.js
@@ -2,10 +2,11 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello Express");
+    absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
 })
 
-//console.log("Hello World");
+console.log("Hello World");
 
 
 
