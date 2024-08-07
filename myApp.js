92931a633757222390484a05483401814ecd7497
talksalot9 <nathanlogan81@gmail.com>
2024-08-06 21:38:13 -0500

    test 8

--- myApp.js
+++ myApp.js
@@ -25,7 +25,12 @@ app.get('/json', function(req, res) {
     res.json({"message": msg});
 })
 
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({"time": req.time});
+});
 
 console.log("Hello World");
 
